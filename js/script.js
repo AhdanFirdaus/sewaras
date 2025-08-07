0b819f21ac8e05f368991f6c0d86f04287307bbb
ahdanfirdaus <muhammadahdanf1@gmail.com>
2025-08-07 07:20:37 +0700

    📌 chore(ui): a little adjustment of navbar and button

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const toggle = document.getElementById('menu-toggle');
 const navLinks = document.getElementById('nav-links');
+const menuIcon = document.getElementById('menu-icon');
 
 toggle.addEventListener('click', () => {
   navLinks.classList.toggle('show');
+  menuIcon.classList.toggle('bi-list');
+  menuIcon.classList.toggle('bi-x');
 });
