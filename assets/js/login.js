05501371b674cf84e4589b41e7429d437a8065d7
ahdanfirdaus <muhammadahdanf1@gmail.com>
2025-10-02 09:31:37 +0700

    ✨ feat(ui): add login page

---
+++ assets/js/login.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+  $(".input-field")
+    .on("focus", function () {
+      $(this).addClass("active");
+    })
+    .on("blur", function () {
+      if ($(this).val() === "") {
+        $(this).removeClass("active");
+      }
+    });
+
+  $(".toggle").on("click", function () {
+    $("main").toggleClass("sign-up-mode");
+  });
+
+  $(".bullets span").on("click", function () {
+    let index = $(this).data("value");
+
+    $(".image").removeClass("show");
+    $(`.img-${index}`).addClass("show");
+
+    $(".text-group").css(
+      "transform",
+      `translateY(${-(index - 1) * 2.2}rem)`
+    );
+
+    $(".bullets span").removeClass("active");
+    $(this).addClass("active");
+  });
+});
